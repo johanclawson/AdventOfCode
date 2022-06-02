4c1b917547c9330c6a70c43b03bb90792274001f
Johan Claeson <johan.claeson@gmail.com>
2022-06-02 15:53:19 +0200

    Day 5 and 6

---
+++ Day5.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class Day5
+    {
+        const string FILENAME = "5sample.txt";
+        public static int Part1()
+        {
+            var matrixSize = 10;
+            var matrix = new int[matrixSize, matrixSize];
+
+            var inputRows = File.ReadAllLines(FILENAME).Select(a => a.Split("->"))
+                .Select(a => new
+                {
+                    StartLine = a[0].Trim().Split(","),
+                    EndLine = a[1].Trim().Split(",")
+                })
+                .Select(a => new SimpleCoordinate
+                {
+                    X1 = int.Parse(a.StartLine[0]),
+                    X2 = int.Parse(a.EndLine[0]),
+                    Y1 = int.Parse(a.StartLine[1]),
+                    Y2 = int.Parse(a.EndLine[1])
+                })
+                .Where(a => a.X1 == a.X2 || a.Y1 == a.Y2)
+                .ToList();
+
+            inputRows.Where(a => a.Y1 == a.Y2).ToList().ForEach(a =>
+            {
+                var steps = Math.Abs(a.X2 - a.X1);
+                var lowest = a.X1 < a.X2 ? a.X1 : a.X2;
+                for (int i = lowest; i <= lowest + steps; i++)
+                {
+                    matrix[i, a.Y1]++;
+                }
+            });
+
+            inputRows.Where(a => a.X1 == a.X2).ToList().ForEach(a =>
+            {
+                var steps = Math.Abs(a.Y2 - a.Y1);
+                var lowest = a.Y1 < a.Y2 ? a.Y1 : a.Y2;
+                for (int i = lowest; i <= lowest + steps; i++)
+                {
+                    matrix[a.X1, i]++;
+                }
+            });
+
+            var cnt = 0;
+            for (int x = 0; x < matrixSize; x++)
+                for (int y = 0; y < matrixSize; y++)
+                    if (matrix[x,y] > 1) 
+                        cnt++;
+
+
+            return cnt;
+
+        }
+
+
+        public static int Part2()
+        {
+            return -1;
+        }
+
+    }
+
+    internal class SimpleCoordinate
+    {
+        public int X1 { get; set; }
+        public int X2 { get; set; }
+        public int Y1 { get; set; }
+        public int Y2 { get; set; }
+    }
+}
---
+++ Day6.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class Day6
+    {
+        const string FILENAME = "6sample.txt";
+        public static int Part1()
+        {
+            var startDays = File.ReadAllText(FILENAME).Split(",").Select(a => int.Parse(a));
+            foreach (var day in startDays)
+            {
+                new LanternFish(day);
+            }
+
+            for (int i = 0; i < 80; i++)
+            {
+                LanternFish.NewDayBreed();
+            }
+
+            return LanternFish.GetNumFishes();
+        }
+
+
+        public static int Part2()
+        {
+            return -1;
+        }
+
+        class LanternFish
+        {
+            static List<LanternFish> fishList = new List<LanternFish>();
+            static int _day;
+
+            int _daysToSpawn = 0;
+            public LanternFish(int daysToSpawn)
+            {
+                _daysToSpawn = daysToSpawn;
+                fishList.Add(this);
+            }
+
+            //A lanternfish that creates a new fish resets its timer to 6, not 7 (because 0 is included as a valid timer value).
+            //The new lanternfish starts with an internal timer of 8 and does not start counting down until the next day.
+            public static void NewDayBreed()
+            {
+                var numOfFishesToAdd = 0;
+                foreach (var fish in fishList)
+                {
+                    if (fish._daysToSpawn == 0)
+                    {
+                        fish._daysToSpawn = 6;
+                        numOfFishesToAdd++;
+                        
+                    }
+                    else
+                    {
+                        fish._daysToSpawn--;
+                    }
+                }
+
+                // Add new fishes - not allowed to modify list while enumerating
+                for (int i = 0; i < numOfFishesToAdd; i++)
+                {
+                    new LanternFish(8);
+                }
+
+                _day++;
+
+
+            }
+
+            public static int GetNumFishes()
+            {
+                return fishList.Count;
+            }
+        }
+
+    }
+
+}
